38261162c6c3fe24301136444c3da4c83362bd35
Anton898 <81709784+IvanovAnto@users.noreply.github.com>
2023-08-16 16:43:51 +0300

    addHM

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+function onTimeupdate({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+const time = localStorage.getItem(STORAGE_KEY) || 0;
+console.log(time);
+
+player.setCurrentTime(time);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+let formState = {};
+const formEl = document.querySelector('.feedback-form');
 
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmit);
+function onInput(e) {
+  const { name, value } = e.target;
+  formState[name] = value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(formState);
+  formState = {};
+  localStorage.removeItem(STORAGE_KEY);
+  e.target.reset();
+}
+
+function onLoad() {
+  try {
+    const data = localStorage.getItem(STORAGE_KEY);
+    if (!data) return;
+    console.log(data);
+    formState = JSON.parse(data);
+    console.log(formState);
+    console.log(Object.entries(formState));
+    Object.entries(formState).forEach(([key, value]) => {
+      formEl.elements[key].value = value;
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+window.addEventListener('load', onLoad);
